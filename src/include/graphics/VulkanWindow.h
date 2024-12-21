f4fb862a8e2b29c290003741a3872da38608155a
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2024-12-21 11:38:12 -0600

    Update uniform buffer

--- src/include/graphics/VulkanWindow.h
+++ src/include/graphics/VulkanWindow.h
@@ -1354,10 +1354,11 @@ public:
         float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
         UniformBufferObject ubo{};
-        ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.view = glm::lookAt(glm::vec3(0.01f, 0.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f));
-        ubo.proj = glm::perspective(glm::radians(90.0f), m_SwapChainExtent2.width / (float)m_SwapChainExtent2.height, 0.1f, 10.0f);
-        ubo.proj[1][1] *= -1;
+        // ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.model = glm::mat4(1.0f);
+        ubo.view = glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        ubo.proj = glm::perspective(glm::radians(90.0f), 640.0f / 480.0f, 0.1f, 10.0f); // m_SwapChainExtent2.width / (float)m_SwapChainExtent2.height
+        // ubo.proj[1][1] *= -1;
 
         void *data;
         vkMapMemory(m_Device, m_UniformBuffersMemory2[currentImage], 0, sizeof(ubo), 0, &data);
@@ -1450,5 +1451,13 @@ public:
         {{0.0f, 0.0f, -0.5f}, {1.0f, 0.0f, 0.0f, 0.0f}, {-1.0f, -1.0f}},
         {{1.0f, 0.0f, 0.5f}, {0.0f, 1.0f, 0.0f, 0.0f}, {-1.0f, -1.0f}},
         {{1.0f, 1.0f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f}, {-1.0f, -1.0f}},
+
+        {{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f, 0.0f}, {-1.0f, -1.0f}},
+        {{0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}, {-1.0f, -1.0f}},
+        {{0.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 1.0f, 1.0f}, {-1.0f, -1.0f}},
+
+        {{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f, 0.0f}, {-1.0f, -1.0f}},
+        {{0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 0.0f}, {-1.0f, -1.0f}},
+        {{1.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f, 1.0f}, {-1.0f, -1.0f}},
     };
 };
